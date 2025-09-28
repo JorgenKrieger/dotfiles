102f855b3a7cf91ae304ffb183c3a667d26338ee
JorgenKrieger <jorgen@labelnoir.me>
2025-09-28 22:13:28 +0200

    Removed more old configs

--- shell/config.d/linux.sh
+++
@@ -1,6 +0,0 @@
-# Remove the very annoying linux background colors
-if [[ ! "$(uname)" == "Darwin" ]]; then 
-	eval "$(dircolors -p | \
-		sed 's/ 4[0-9];/ 01;/; s/;4[0-9];/;01;/g; s/;4[0-9] /;01 /' | \
-		dircolors /dev/stdin)"
-fi
